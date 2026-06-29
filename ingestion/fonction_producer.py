58a68572b2e59c22e39527177ef838564b68b79c
Carlot Steven <steven.carlotpro@gmail.com>
2026-06-29 10:12:58 +0200

    Modification du fonctionnement des producer, suppression de la mecanique fallback, ecriture et utilisation de binance et coinbase simultanement

--- ingestion/fonction_producer.py
+++ ingestion/fonction_producer.py
@@ -3,10 +3,9 @@ import websockets
 import json
 import time
 import logging
-from datetime import datetime
 from kafka import KafkaProducer
 from kafka.admin import KafkaAdminClient, NewTopic
-from kafka.errors import KafkaError, TopicAlreadyExistsError
+from kafka.errors import TopicAlreadyExistsError
 
 logger = logging.getLogger("ingestion")
 
@@ -105,16 +104,14 @@ async def coinbase_consumer(
                     # Mettre à jour l'horodatage pour prouver que Coinbase est en vie
                     shared_state["last_coinbase_time"] = time.time()
 
-                    if shared_state["active_source"] == "coinbase":
-                        key_val = data.get("trade_id")
-                        price_val = data.get("price")
+                    key_val = data.get("trade_id")
+                    price_val = data.get("price")
 
-                        if key_val is not None and price_val is not None:
-                            producer.send(
-                                topic=topic_name,
-                                key=str(key_val).encode("utf-8"),
-                                value=data,
-                            )
+                    producer.send(
+                        topic=topic_name,
+                        key=str(key_val).encode("utf-8"),
+                        value=data,
+                    )
         except websockets.ConnectionClosed:
             logger.warning(
                 "[Coinbase] Connexion fermée, tentative de reconnexion dans 5 secondes..."
@@ -137,16 +134,14 @@ async def binance_consumer(
                 async for message in websocket:
                     data = json.loads(message)
 
-                    if shared_state["active_source"] == "binance":
-                        key_val = data.get("E")  # Event time
-                        price_val = data.get("p")  # Price
+                    key_val = data.get("E")  # Event time
+                    price_val = data.get("p")  # Price
 
-                        if key_val is not None and price_val is not None:
-                            producer.send(
-                                topic=topic_name,
-                                key=str(key_val).encode("utf-8"),
-                                value=data,
-                            )
+                    producer.send(
+                        topic=topic_name,
+                        key=str(key_val).encode("utf-8"),
+                        value=data,
+                    )
         except websockets.ConnectionClosed:
             logger.warning(
                 "[Binance] Connexion fermée, tentative de reconnexion dans 5 secondes..."
@@ -155,29 +150,6 @@ async def binance_consumer(
             logger.error(f"[Binance] Erreur: {e}", exc_info=True)
 
         await asyncio.sleep(5)
-
-
-async def monitor_health(shared_state, timeout=3.0):
-    logger.info(f"[Monitor] Démarrage de la surveillance (timeout={timeout}s)")
-    while True:
-        now = time.time()
-        elapsed = now - shared_state["last_coinbase_time"]
-
-        if elapsed > timeout:
-            if shared_state["active_source"] != "binance":
-                logger.warning(
-                    f"[Monitor] !!! Coinbase inactif depuis {elapsed:.2f}s. Bascule sur Binance !!!"
-                )
-                shared_state["active_source"] = "binance"
-        else:
-            if shared_state["active_source"] != "coinbase":
-                logger.info(
-                    f"[Monitor] *** Coinbase a repris après {elapsed:.2f}s. Retour sur Coinbase ***"
-                )
-                shared_state["active_source"] = "coinbase"
-
-        await asyncio.sleep(0.5)
-
 
 async def producer_stream(
     topic_name: list[str] = ["coinbase.btc.usd.trades", "binance.btc.usd.trades"],
@@ -214,8 +186,7 @@ async def producer_stream(
         # Lancement simultané des trois tâches concourantes
         await asyncio.gather(
             coinbase_consumer(shared_state, producer, coinbase_url),
-            binance_consumer(shared_state, producer, binance_url),
-            monitor_health(shared_state, timeout=2.0),
+            binance_consumer(shared_state, producer, binance_url)
         )
     except asyncio.CancelledError:
         logger.info("Tâches annulées par l'utilisateur")
